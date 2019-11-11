5fb42d660cb73f0f33efeb09a4a0fd6a3ceb1743
killerisland <57371055+killerisland@users.noreply.github.com>
2019-11-11 19:10:40 +0000

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('Killer Island Home', { title: 'By Izzy and Nat' });
 });
 
 module.exports = router;
