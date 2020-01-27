3d674f1a1945499f6ff33f56ba477f1079990f38
@DaveBlakeman <pdb@rpsys.co.uk>
2020-01-27 19:49:59 +0000

    Update app.js

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 //Database connection
 app.use(function(req, res, next) {
 	res.locals.connection = mysql.createConnection({
-		host     : 'remotemysql.com',
+		host     : 'remotemysql.com', 
 		user     : 'MEmh29aO0i',
 		password : 'nJgXhfwt4J',
 		database : 'MEmh29aO0i'
