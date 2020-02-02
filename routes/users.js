cf1809ef27974e809c0e10e035b4ad526c9b10a9
@DaveBlakeman <pdb@rpsys.co.uk>
2020-02-02 14:23:11 +0000

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -3,15 +3,21 @@ var router = express.Router();
 
 function HandleSqlRequest(sql, req, res, next) {
 	console.log('Sql = ' + sql)
-	res.locals.connection.query(sql, function (error, results, fields) {
-	  	if (error){
-	  		res.send(JSON.stringify({"status": 500, "error": error, "response": null})); 
-	  		//If there is error, we send the error in the error section with 500 status
-	  	} else {
-  			res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
-  			//If there is no error, all is good and response is 200OK.
-	  	}
-	});
+	try {
+		res.locals.connection.query(sql, function (error, results, fields) {
+			if (error){
+				res.send(JSON.stringify({"status": 500, "error": error, "response": null})); 
+				//If there is error, we send the error in the error section with 500 status
+			} else {
+				res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
+				//If there is no error, all is good and response is 200OK.
+			}
+		});
+	}
+	catch (err) {
+		res.send(JSON.stringify({"status": 500, "error": err, "response": null}));
+	}
+		
 }
 
 function HandleSqlUpdate(sql, args, req, res) {
